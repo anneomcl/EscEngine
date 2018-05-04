bf561821cc56bf2976b1726efa3c61630f79182f
Anne <anneomclaughlin@gmail.com>
2018-05-03 22:48:36 -0700

    Creating nuget package configs

--- EscEngine/Properties/AssemblyInfo.cs
+++ EscEngine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EscEngine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EsckieEngine")]
+[assembly: AssemblyDescription("EsckieEngine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EscEngine")]
+[assembly: AssemblyCompany("HappieCat Studios")]
+[assembly: AssemblyProduct("EsckieEngine")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
