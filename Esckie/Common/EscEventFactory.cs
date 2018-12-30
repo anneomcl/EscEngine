2c9d8ae27f1f05224cd8e948cbc36de715a6f5c8
Anne <anneomclaughlin@gmail.com>
2018-12-30 15:44:56 -0800

    Cleaning up recursion in event factory

--- Esckie/Common/EscEventFactory.cs
+++ Esckie/Common/EscEventFactory.cs
@@ -11,6 +11,7 @@ namespace Esckie.Common
         {
             public static string If => "if";
             public static string Else => "else";
+            public static string ElseIf => "elif";
         };
 
         public static EscEvent Create(string eventName, List<string> lines, Dictionary<string, ActionMetadata> actions)
@@ -25,27 +26,57 @@ namespace Esckie.Common
 
         private static EscCommand ConvertScriptToLogicTree(int rootIndentLevel, List<string> lines, EscCommand root, Dictionary<string, ActionMetadata> actions)
         {
-            if (lines.Count == 0)
+            // Base cases: 1) end of file, 2) child block returning to parent, or 3) end of current event.
+            if (lines.Count == 0 ||
+                EscCompilerHelpers.GetIndentationLevel(lines.First()) < rootIndentLevel ||
+                EscCompilerHelpers.IsEventProcessingFinished(lines.First()))
             {
                 return root;
             }
 
+            // Validate the line is parseable
             var line = lines.First();
             var indentLevel = EscCompilerHelpers.GetIndentationLevel(line);
-
-            if (StringExtensions.IsNullOrWhiteSpace(line))
+            if (!ValidateSyntax(line, lines))
             {
+                return ConvertScriptToLogicTree(indentLevel, lines, root, actions);
+            }
+
+            // Process child blocks
+            var tokens = EscCompilerHelpers.ParseLineToTokens(line);
+            var action = tokens.First();
+            if (action == Keywords.If || action == Keywords.Else || action == Keywords.ElseIf)
+            {
+                ProcessControlLogic(action, tokens, indentLevel, lines, root, actions);
+            }
+
+            // Process custom actions
+            if (actions.Keys.Contains(action))
+            {
+                root.Children.Add(EscCommandFactory.Create(tokens, actions));
                 lines.RemoveAt(0);
                 return ConvertScriptToLogicTree(indentLevel, lines, root, actions);
             }
-            if (indentLevel < rootIndentLevel ||
-                EscCompilerHelpers.IsEventProcessingFinished(line))
+            else
             {
-                return root;
+                throw new InvalidOperationException($"There is no action {action} available.");
             }
+        }
 
-            var tokens = EscCompilerHelpers.ParseLineToTokens(line);
-            var action = tokens.First();
+        private static KeyValuePair<string, bool> ParseCondition(string condition)
+        {
+            if (condition[0] == '!')
+            {
+                return new KeyValuePair<string, bool>(condition.Substring(1), false);
+            }
+            else
+            {
+                return new KeyValuePair<string, bool>(condition, true);
+            }
+        }
+
+        private static void ProcessControlLogic(string action, List<string> tokens, int indentLevel, List<string> lines, EscCommand root, Dictionary<string, ActionMetadata> actions)
+        {
             if (action == Keywords.If)
             {
                 var condition = ParseCondition(tokens[1]);
@@ -64,7 +95,7 @@ namespace Esckie.Common
             {
                 if (root.Children.Last().Name != Keywords.If)
                 {
-                    throw new Exception();
+                    throw new InvalidOperationException("No valid 'if' block paired with 'else'.");
                 }
 
                 var conditions = root.Children.Last().Conditions.Select(x => new KeyValuePair<string, bool>(x.Key, !x.Value)).ToDictionary(x => x.Key, x => x.Value);
@@ -78,26 +109,21 @@ namespace Esckie.Common
                 lines.RemoveAt(0);
                 root.Children.Add(ConvertScriptToLogicTree(indentLevel + 1, lines, currRoot, actions));
             }
-
-            if (actions.Keys.Contains(action))
-            {
-                root.Children.Add(EscCommandFactory.Create(tokens, actions));
-                lines.RemoveAt(0);
-            }
-
-            return ConvertScriptToLogicTree(indentLevel, lines, root, actions);
-        }
-
-        private static KeyValuePair<string, bool> ParseCondition(string condition)
-        {
-            if (condition[0] == '!')
-            {
-                return new KeyValuePair<string, bool>(condition.Substring(1), false);
-            }
             else
             {
-                return new KeyValuePair<string, bool>(condition, true);
+                throw new NotImplementedException();
             }
+        }
+
+        private static bool ValidateSyntax(string line, List<string> lines)
+        {
+            if (StringExtensions.IsNullOrWhiteSpace(line))
+            {
+                lines.RemoveAt(0);
+                return false;
+            }
+
+            return true;
         }
     }
 }
